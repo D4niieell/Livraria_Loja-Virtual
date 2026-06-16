47e592d12606384d7959f793391ec5c9190d8782
D4niieell <danielbarroscedaca@gmail.com>
2026-06-15 21:32:24 -0300

    ...

---
+++ Back-End/src/middlewares/validateUser.js
@@ -0,0 +1,66 @@
+const validateUser = (req, res, next) => {
+  const {
+    user_name,
+    user_email,
+    user_password,
+    user_phone,
+    role_id,
+    user_status,
+  } = req.body;
+
+  let errors = [];
+
+  const newName = user_name.trim();
+  const newEmail = user_email.trim();
+  const newPhone = user_phone.trim();
+  const newPassword = user_phone.trim();
+
+  const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  if (!newName) {
+    errors.push("O nome é obrigatório!");
+  } else if (newName.length < 3 || newName.length > 150) {
+    errors.push("O nome deve ter entre 3 e 100 caracteres!");
+  }
+
+  if (!newEmail) {
+    errors.push("O e-mail é obrigatório!");
+  } else {
+    if (!regexEmail.test(newEmail)) {
+      errors.push("Este e-mail está inválido");
+    } else if (newEmail.length > 150) {
+      errors.push("O e-mail deve ter no máximo 150 caracteres!");
+    }
+  }
+
+  if (!newPhone) {
+    errors.push("O número do telefone é obrigatório!");
+  } else if (newPhone.length != 15) {
+    errors.push("O telefone deve ter apenas 15 caracteres!");
+  }
+
+  if (!newPassword) {
+    errors.push("A senha é obrigatório!");
+  } else if (!regexPassword.test(newPassword)) {
+    errors.push(
+      "A senha deve ter pelo menos 8 caracteres, contendo uma letra maiúscula, e minúscula, número e caractere especial"
+    );
+  }
+
+  if (!role_id) {
+    errors.push("A regra é obrigatória!");
+  }
+
+  if (!user_status) {
+    errors.push("O status é obrigatório!");
+  }
+
+  if (errors.length > 0) {
+    return res.status(400).json({ errors });
+  }
+
+  next();
+};
+
+export default validateUser;
