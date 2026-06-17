60270bf20d54633da0768ebeb0da699308eb06a4
D4niieell <danielbarroscedaca@gmail.com>
2026-06-16 22:31:43 -0300

    ...

--- Back-End/server.js
+++ Back-End/server.js
@@ -9,13 +9,21 @@ import routerCompras from "./src/routes/comprasRouter.js";
 import routerCategoria from "./src/routes/categoriaRoutes.js";
 import routerEditora from "./src/routes/editoraRoutes.js";
 import userRouter from "./src/routes/userRoutes.js";
+import coockieParser from "cookie-parser";
+import loginRoute from "./src/routes/loginRoutes.js";
 
 const app = express();
 
 const PORT = process.env.PORT_SERVER || 8000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:8000",
+    credentials: true, // obrigatório para cookies funcionarem
+  })
+);
 app.use(express.json());
+app.use(coockieParser());
 
 app.use("/clientes", routeCliente);
 app.use("/livros", routerLivros);
@@ -23,6 +31,7 @@ app.use("/compras", routerCompras);
 app.use("/categorias", routerCategoria);
 app.use("/editoras", routerEditora);
 app.use("/users", userRouter);
+app.use("/auth", loginRoute);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em ${PORT}`);
---
+++ Back-End/src/controllers/authLoginController.js
@@ -0,0 +1,59 @@
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import generateTokens from "../utils/generateTokens.js";
+import tokenModels from "../models/tokenModels.js"; 
+import userModels from "../models/userModels.js";
+
+class AuthLoginController {
+    async login (req, res) {
+        const { user_email, user_password } = req.body;
+
+        const [emailExists] = await userModels.selectUserByEmail(user_email);
+
+        if (!emailExists) {
+            return res.ststus(400).json({
+                error: "Email ou senha são inválidos!",
+            });
+        }
+
+        const validatePassword = await bcrypt.compare(
+            user_password, emailExists.user_password
+        );
+
+        if (!validatePassword) {
+            return res.ststus(400).json({
+                error: "Esta senha é inválida!",
+            });
+        }
+
+        const accessToken = generateTokens.generateAccesssToken(emailExists);
+        const refreshToken = generateTokens.generateRefreshToken(emailExists);
+
+        const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // +7 dias
+        
+        const savedToken = await tokenModels.createToken({
+            user_id: emailExists.user_id,
+            token: refreshToken,
+            expires_at: expiresAt
+        })
+
+        if (savedToken.affectedRows === 0) {
+            return res.status(500).json({
+                error: "Erro ao tentar criar token!"
+            })
+        }
+
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: process.env.COOKIE_SECRET,
+            sameSite: "strict",
+            maxAge: 604800000,
+        });
+
+        return res.json({
+            success: "Login realizado com sucesso!",
+            accessToken
+        })
+    }
+}
+export default new AuthLoginController();
